9b113b4af54c2b794f34005369ea5993cc12cba4
Sergey Bukarev <SergeyBukarev@yandex.ru>
2018-06-21 18:36:46 +0300

    state keys

--- app/src/main/java/com/bukarev/camerahelper/CameraConstants.java
+++ app/src/main/java/com/bukarev/camerahelper/CameraConstants.java
@@ -3,6 +3,13 @@ package com.bukarev.camerahelper;
 
 public class CameraConstants {
     public final static String PHOTO_URI_STATE = "com.bukarev.camerahelper.PHOTO_URI_STATE";
+    public final static String IS_SELFIE_STATE = "com.bukarev.camerahelper.IS_SELFIE_STATE";
+    public final static String IS_INTERNAL_STATE = "com.bukarev.camerahelper.IS_INTERNAL_STATE";
+    public final static String CAMERA_PERMISSIONS_REQUEST_CODE = "com.bukarev.camerahelper.CAMERA_PERMISSIONS_REQUEST_CODE";
+    public final static String STORAGE_PERMISSIONS_REQUEST_CODE = "com.bukarev.camerahelper.STORAGE_PERMISSIONS_REQUEST_CODE";
+    public final static String PHOTO_REQUEST_CODE = "com.bukarev.camerahelper.PHOTO_REQUEST_CODE";
+    public final static String GALLERY_REQUEST_CODE = "com.bukarev.camerahelper.GALLERY_REQUEST_CODE";
+
     public final static String AUTHORITY = "com.bukarev.camerahelper.fileprovider";
     public final static String SELFIE_PARAM1 = "android.intent.extras.CAMERA_FACING";
     public final static String SELFIE_PARAM2 = "android.intent.extra.USE_FRONT_CAMERA";
