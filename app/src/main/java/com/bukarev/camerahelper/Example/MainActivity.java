27c7f42b16327b71fb8204253701e98b42d7e4b8
Sergey Bukarev <SergeyBukarev@yandex.ru>
2018-06-22 12:32:09 +0300

    example

--- app/src/main/java/com/bukarev/camerahelper/Example/MainActivity.java
+++ app/src/main/java/com/bukarev/camerahelper/Example/MainActivity.java
@@ -5,23 +5,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
-import com.bukarev.camerahelper.CameraHelper.CameraHelper;
 import com.bukarev.camerahelper.CameraHelper.CameraErrorInterface;
+import com.bukarev.camerahelper.CameraHelper.CameraHelper;
 import com.bukarev.camerahelper.R;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements CameraErrorInterface {
     private CameraHelper cameraHelper;
+    private static final int PHOTO_REQUEST_CODE = 1;
+    private static final int GALLERY_REQUEST_CODE = 2;
+    private static final int CAMERA_PERMISSION_CODE = 3;
+    private static final int STORAGE_PERMISSION_CODE = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraHelper = CameraHelper.newBuilder(this, 1, 2, 3, 4)
+        cameraHelper = CameraHelper.newBuilder(this, PHOTO_REQUEST_CODE, GALLERY_REQUEST_CODE, CAMERA_PERMISSION_CODE, STORAGE_PERMISSION_CODE)
                 .setErrorInterface(this)
                 .build();
-        cameraHelper.startCameraWithPermission(true, false);
+        //cameraHelper.startCameraWithPermission(true, false);
         cameraHelper.startPickingFromGallery();
     }
 
@@ -40,8 +44,9 @@ public class MainActivity extends AppCompatActivity implements CameraErrorInterf
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        File file = cameraHelper.onActivityResult(requestCode, resultCode, data);
-        String qwe = "";
+        if ((requestCode == PHOTO_REQUEST_CODE || requestCode == GALLERY_REQUEST_CODE) && resultCode == RESULT_OK) {
+            File file = cameraHelper.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -52,6 +57,6 @@ public class MainActivity extends AppCompatActivity implements CameraErrorInterf
 
     @Override
     public void onCameraError(Throwable error) {
-
+        // handle error
     }
 }
